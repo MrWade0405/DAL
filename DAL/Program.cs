14a61485605cd7657fa49d0ffabb3bd451d9ae2a
Vitalii Mahlona <maglona2013@gmail.com>
2019-05-22 17:24:23 +0300

    added models and context

---
+++ DAL/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace DAL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder();
+            // установка пути к текущему каталогу
+            builder.SetBasePath(Directory.GetCurrentDirectory());
+            // получаем конфигурацию из файла appsettings.json
+            builder.AddJsonFile("appsettings.json");
+            // создаем конфигурацию
+            var config = builder.Build();
+            // получаем строку подключения
+            string connectionString = config.GetConnectionString("DefaultConnection");
+
+            var optionsBuilder = new DbContextOptionsBuilder<LearningLanguagesContext>();
+            var options = optionsBuilder
+                .UseSqlServer(connectionString)
+                .Options;
+
+            using (LearningLanguagesContext db = new LearningLanguagesContext(options))
+            {
+               
+            }
+            Console.ReadKey();
+        }
+    }
+}
